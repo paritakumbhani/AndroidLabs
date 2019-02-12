00ad92b5b874ee669398dca5bae14ae846ea7d7a
Parita Kumbhani <kumb0004@algonquinlive.com>
2019-02-12 10:56:52 -0500

    lab 3 update

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -11,34 +11,35 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     SharedPreferences share;
     EditText emailField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
 
-        Button nextButton = (Button)findViewById(R.id.button);
-        emailField = (EditText)findViewById(R.id.email);
-        share = getSharedPreferences("FileName", Context.MODE_PRIVATE);
-        String saveEmail = share.getString("ReserveName", "");
-        emailField.setText(saveEmail);
-        nextButton.setOnClickListener( b -> {
+        Button nextButton = (Button) findViewById (R.id.button);
+        emailField = (EditText) findViewById (R.id.email);
+        share = getSharedPreferences ("FileName", Context.MODE_PRIVATE);
+        String saveEmail = share.getString ("ReserveName", "");
+        emailField.setText (saveEmail);
+        nextButton.setOnClickListener (b -> {
 
             //Give directions to go from this page, to ProfileActivity
-            Intent nextPage = new Intent(MainActivity.this, ProfileActivity.class);
+            Intent nextPage = new Intent (MainActivity.this, ProfileActivity.class);
 
             //   EditText et =(EditText)findViewById(R.id.)
-            nextPage.putExtra("emailType", emailField.getText().toString());
+            nextPage.putExtra ("emailType", emailField.getText ( ).toString ( ));
             //Now make the transition:
-            startActivityForResult(nextPage, 345);
+            startActivityForResult (nextPage, 345);
         });
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
-        SharedPreferences.Editor editor = share.edit();
-        String typeEmail  = emailField.getText().toString();
-        editor.putString("ReserveName", typeEmail);
-        editor.commit();
+        super.onPause ( );
+        SharedPreferences.Editor editor = share.edit ( );
+        String typeEmail = emailField.getText ( ).toString ( );
+        editor.putString ("ReserveName", typeEmail);
+        editor.commit ( );
     }
 }
